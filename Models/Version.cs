afbf59eb43ca5ff6dda684d53eff24a8a0c99a33
voltara13 <stalker.nik20@gmail.com>
2022-11-24 23:30:47 +0700

    Added gitignore

---
+++ Models/Version.cs
@@ -0,0 +1,9 @@
+﻿namespace LW1.Models
+{
+    public class Version
+    {
+        public string Company { get; set; }
+        public string ProductVersion { get; set; }
+        public string Product { get; set; }
+    }
+}
--- obj/Debug/netcoreapp2.1/LW1.AssemblyInfo.cs
+++ obj/Debug/netcoreapp2.1/LW1.AssemblyInfo.cs
@@ -11,13 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LW1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("BibaAndBoba.com")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LW1")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.3.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.3-rc.1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lab 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LW1")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.0.3.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
