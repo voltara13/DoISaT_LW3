32c14cd6b4b6ca2a9d5d0f5cf30ae62c348b1099
voltara13 <stalker.nik20@gmail.com>
2022-11-24 22:21:43 +0700

    Первый коммит

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LW1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LW1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private static readonly IList<Car> MemCache = new List<Car>();
+
+        // GET api/<CarsController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Car>> Get()
+        {
+            return MemCache.ToList();
+        }
+
+        // GET api/<CarsController>/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Car> Get(int id)
+        {
+            if (id < 0 || id >= MemCache.Count)
+            {
+                throw new IndexOutOfRangeException("Index out of range");
+            }
+
+            return MemCache[id];
+        }
+
+        // POST api/<CarsController>
+        [HttpPost]
+        public void Post([FromBody] Car value)
+        {
+            MemCache.Add(value);
+        }
+
+        // PUT api/<CarsController>/{id}
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Car value)
+        {
+            if (id < 0 || id >= MemCache.Count)
+            {
+                throw new IndexOutOfRangeException("Index out of range");
+            }
+
+            MemCache[id] = value;
+        }
+
+        // DELETE api/<CarsController>/{id}
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id < 0 || id >= MemCache.Count)
+            {
+                throw new IndexOutOfRangeException("Index out of range");
+            }
+
+            MemCache.RemoveAt(id);
+        }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace LW1.Models
+{
+    public class Car
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double EngineVolume { get; set; }
+    }
+}
